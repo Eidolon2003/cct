ef01c5aef5bdb95194dba94d6e27ebc07158fffb
Julian Williams <julian@wazworld.net>
2025-06-02 20:09:04 -0500

    initial mining setup; more bugfixes to come

---
+++ DW1.21/miningserver.lua
@@ -0,0 +1,243 @@
+--These variables are file backed:
+local currentCell = vector.new(0,0,0)
+local miningQueue = {}
+local inProgress = {}
+
+--These are not:
+local FILENAME = "mine.dat"
+
+local function sizeof(tbl)
+	local x = 0
+	for k,v in pairs(tbl) do
+		x = x + 1
+	end
+	return x
+end
+
+local function readVector(file)
+	local x = file.read()
+	local y = file.read()
+	local z = file.read()
+	return vector.new(x,y,z)
+end
+
+local function readFile()
+	local file = fs.open(FILENAME, "rb")
+	if not file then return nil end
+	
+	currentCell = readVector(file)
+	
+	local miningQueueSize = file.read()
+	for i = 1,miningQueueSize do
+		local v = readVector(file)
+		table.insert(miningQueue, v)
+	end
+	
+	local inProgressSize = file.read()
+	for i = 1,inProgressSize do
+		local id = file.read()
+		local v = readVector(file)
+		inProgress[id] = v
+	end
+	
+	file.close()
+	return true
+end
+
+local function writeVector(file, v)
+	file.write(v.x)
+	file.write(v.y)
+	file.write(v.z)
+end
+
+local function writeFile()
+	local file = fs.open(FILENAME, "w+b")
+	
+	writeVector(file, currentCell)
+	
+	file.write(sizeof(miningQueue))
+	for i,v in ipairs(miningQueue) do
+		writeVector(file, v)
+	end
+	
+	file.write(sizeof(inProgress))
+	for i,v in pairs(inProgress) do
+		file.write(i)
+		writeVector(file, v)
+	end
+end
+
+local function getNextCell()
+	local nextCell = currentCell + vector.new(0,0,1)
+	if nextCell.z == 16 then
+		nextCell.z = 0
+		nextCell = nextCell + vector.new(1,0,0)
+	end
+	if nextCell.x == 16 then
+		return nil
+	else
+		return nextCell
+	end
+end
+
+local function getNextCoordinate()
+	if #miningQueue == 0 then
+		--"knight's move" pattern
+		local offsets = {
+			vector.new(0,0,1),
+			vector.new(1,0,4),
+			vector.new(2,0,2),
+			vector.new(3,0,5),
+			vector.new(4,0,3),
+		}
+		
+		for i,offset in ipairs(offsets) do
+			table.insert(
+				miningQueue,
+				offset + (currentCell * 5)
+			)
+		end
+		
+		currentCell = getNextCell()
+	end
+	
+	return table.remove(miningQueue, 1)
+end
+
+local function dataToVec(data)
+	return vector.new(data.x, data.y, data.z)
+end
+
+local function removeByValue(tbl, val)
+	for k,v in pairs(tbl) do
+		if val == v then
+			tbl[k] = nil
+			return
+		end
+	end
+end
+
+local turtles = {}
+
+local STATUS_HOMING = "homing"
+local STATUS_UNLOAD = "unloading"
+local STATUS_MINING = "mining"
+local STATUS_IDLE = "idle"
+local STATUS_NEW = "new"
+local MSG_HOME = "home"
+local MSG_MINE = "mine"
+local MSG_IDLE = "idle"
+
+local function sendMessage(id, msg, data)
+	x = {}
+	x.msg = msg
+	x.data = data
+	jpi.send(id, x)
+end
+
+local function handleMessage(senderID, msg)
+	local oldStatus	= turtles[senderID]
+	
+	local newMine = function()
+		local nextCoord = getNextCoordinate()
+		local msg = nil
+		if nextCoord then
+			msg = MSG_MINE
+			turtles[senderID] = STATUS_MINING
+			inProgress[senderID] = nextCoord
+		else
+			msg = MSG_IDLE
+			turtles[senderID] = STATUS_IDLE
+		end
+		sendMessage(senderID, msg, nextCoord)
+	end
+	
+	if oldStatus == STATUS_HOMING then
+		if inProgress[senderID] then
+			turtles[senderID] = STATUS_MINING
+			sendMessage(
+				senderID,
+				MSG_MINE,
+				inProgress[senderID]
+			)
+		else
+			newMine()
+		end
+	elseif oldStatus == STATUS_UNLOAD then
+		newMine()
+	elseif oldStatus == STATUS_MINING then
+		turtles[senderID] = STATUS_UNLOAD
+		local v = dataToVec(msg)
+		removeByValue(inProgress, v)
+	end
+	
+	writeFile()
+end
+
+local function main()
+	--Wait a hot sec for turtles to connect
+	os.sleep(5)
+
+	turtles = {}
+	readFile()
+	
+	while true do
+		local arp = jpi.getArpCache()
+		
+		--Make sure all our turtles are still here
+		for key,val in pairs(arp) do
+			if not jpi.ping(val) then
+				turtles[val] = nil
+				arp[key] = nil
+			end
+		end
+		
+		--Pick up any new turtles
+		-- Or old turtles that reconnected
+		for key,val in pairs(arp) do
+			if string.find(key, "mt_")
+			and not turtles[val] then
+				turtles[val] = STATUS_NEW
+			end
+		end
+		
+		--Only allow one turtle to home at a time
+		--Start with the nearest one
+		local homeBusy = false
+		for id,status in pairs(turtles) do
+			if status == STATUS_HOMING then
+				homeBusy = true
+			end
+		end
+		
+		if not homeBusy then
+			local lowestDistance = nil
+			local lowestID = nil
+		
+			for id,status in pairs(turtles) do
+				if status == STATUS_NEW then
+					d = jpi.ping(id)
+				
+					if not lowestDistance
+					or d < lowestDistance then
+						lowestDistance = d
+						lowestID = id
+					end
+				end
+			end
+			
+			if lowestID then
+				turtles[lowestID] = STATUS_HOMING
+				sendMessage(lowestID, MSG_HOME)
+			end
+		end
+			
+		while true do
+			senderID, msg = jpi.receive(3)
+			if not senderID then break end
+			handleMessage(senderID, msg)
+		end
+	end
+end
+
+main()
---
+++ DW1.21/miningturtle.lua
@@ -0,0 +1,182 @@
+local DEBUG = false
+
+local MSG_HOME = "home"
+local MSG_MINE = "mine"
+local MSG_IDLE = "idle"
+
+local serverID = jpi.arp("MiningServer")
+if DEBUG then print("arp") end
+while not serverID do
+	if DEBUG then print("arp") end
+	serverID = jpi.arp("MiningServer")
+end
+
+local mineVector = vector.new()
+
+local function dropOff()
+	jpi.moveto(vector.new(0, 0, 0))
+	jpi.face(vector.new(0, 0, -1))
+	for i = 2,16 do
+		turtle.select(i)
+		turtle.drop()
+	end
+	
+	jpi.up()
+	turtle.select(1)
+	turtle.suck(64 - turtle.getItemCount())
+end
+	
+local function home()
+	local dist = jpi.ping(serverID)
+	local oldDist
+	
+	local fg = function(f)
+		repeat
+			oldDist = dist
+			repeat until f()
+			dist = jpi.ping(serverID)
+		until dist > oldDist
+	end
+	
+	local fge = function(f)
+		repeat
+			oldDist = dist
+			f()
+			dist = jpi.ping(serverID)
+		until dist >= oldDist
+	end
+	
+	--Find top of computer
+	fge(turtle.down)
+	if jpi.ping(serverID) == 1 then
+		goto skip
+	end
+	fg(turtle.up)
+	fg(turtle.forward)
+	turtle.turnLeft()
+	turtle.turnLeft()
+	fg(turtle.forward)
+	turtle.turnLeft()
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.turnLeft()
+	fg(turtle.forward)
+	turtle.turnLeft()
+	turtle.turnLeft()
+	fg(turtle.forward)
+	turtle.turnLeft()
+	turtle.turnLeft()
+	turtle.forward()
+::skip::
+	
+	--face opposite dropoff chest
+	repeat until turtle.up()
+	local a,b = turtle.inspect()
+	while not a
+	or string.find(b.name, "turtle") do
+		turtle.turnLeft()
+		a,b = turtle.inspect()
+	end
+	turtle.turnRight()
+	turtle.turnRight()
+	
+	jpi.setOrigin()
+	dropOff()
+	jpi.send(serverID, 0)
+	return
+end
+
+local function isOre()
+	local a,b = turtle.inspect()
+	if not a then return false end
+	
+	local nonores = {
+		"minecraft:andesite",
+		"xycraft_world:kivi",
+	}
+	
+	local flag = false
+	for key,val in pairs(nonores) do
+		if b.name == val then 
+			flag = true 
+			break
+		end
+	end
+	
+	return flag or b.tags["c:ores"]
+end
+
+local function mine()
+	--goto mine
+	jpi.move(mineVector)
+	local block,data = turtle.inspectDown()
+	while not block 
+	or string.find(data.name, "turtle") do
+		jpi.down()
+		block,data = turtle.inspectDown()
+	end
+	
+	--mine
+	while turtle.digDown() do
+		repeat until not turtle.digDown()
+		jpi.down()
+
+		for i = 1,4 do
+			if isOre() then
+				turtle.dig()
+			end
+			jpi.turnRight()
+		end
+		
+		if turtle.getItemDetail(16) then
+			local p = jpi.getPos()
+			dropOff()
+			jpi.move(mineVector)
+			jpi.moveto(p)
+		end
+	end
+	
+	jpi.moveto(vector.new(mineVector.x, -3, mineVector.z))
+	for i = 2,16 do
+		x = turtle.getItemDetail(i)
+		if not x then break end
+		
+		if x.name == "minecraft:cobblestone"
+		or x.name == "minecraft:cobbled_deepslate"
+		then
+			turtle.select(i)
+			turtle.placeDown()
+			break
+		end
+	end
+	
+	jpi.send(serverID, mineVector)
+	dropOff()
+	jpi.send(serverID, 0)
+end
+
+local function dataToVec(data)
+	return vector.new(data.x, data.y, data.z)
+end
+
+local function handleMessage(msg, data)
+	if msg == MSG_HOME then
+		if DEBUG then print("home") end
+		home()
+	elseif msg == MSG_MINE then
+		mineVector = dataToVec(data)
+		if DEBUG then print("mine", mineVector) end
+		mine()
+	end
+end
+
+local function main()
+	while true do
+		senderID, msg = jpi.receive()
+		if senderID == serverID then
+			handleMessage(msg.msg, msg.data)
+		end
+	end
+end
+
+main()
