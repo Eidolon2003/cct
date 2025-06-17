120ef893819491803ded18ed658084c5e99c88f7
Julian Williams <julian@wazworld.net>
2025-06-16 23:32:15 -0500

    update key for new jpi

--- DW1.21/key.lua
+++ DW1.21/key.lua
@@ -1,44 +1,40 @@
-if not jpi then
-	require("jpi").execute("key.lua")
-else
+local function main(jpi)
+	local serverID = nil
+	repeat
+		serverID = jpi.arp("MainServer")
+	until serverID
 
-
-local serverID = nil
-repeat
-	serverID = jpi.arp("MainServer")
-until serverID
-
-local function send(e,a,b)
-	payload = {}
-	payload.event = e
-	payload.first = a
-	payload.second = b
-	jpi.send(serverID, payload)
-end
-
-local function keyHandler()
-	while true do
-		local e,k,h = os.pullEvent("key")
-		send(e,k,h)
+	local function send(e,a,b)
+		payload = {}
+		payload.event = e
+		payload.first = a
+		payload.second = b
+		jpi.send(serverID, payload)
 	end
-end
 
-
-local function keyUpHandler()
-	while true do
-		local e,k = os.pullEvent("key_up")
-		send(e,k,h)
+	local function keyHandler()
+		while true do
+			local e,k,h = os.pullEvent("key")
+			send(e,k,h)
+		end
 	end
-end
 
-local function charHandler()
-	while true do
-		local e,c = os.pullEvent("char")
-		send(e,c)
+
+	local function keyUpHandler()
+		while true do
+			local e,k = os.pullEvent("key_up")
+			send(e,k,h)
+		end
 	end
+
+	local function charHandler()
+		while true do
+			local e,c = os.pullEvent("char")
+			send(e,c)
+		end
+	end
+
+	parallel.waitForAny(keyHandler, keyUpHandler, charHandler)
 end
 
-parallel.waitForAny(keyHandler, keyUpHandler, charHandler)
-
-
-end
+require("jpi")(main)
