9a17d9638d73b65e68b979783971297ab5e45ed3
Julian Williams <julian@wazworld.net>
2025-06-18 14:44:53 -0500

    update display to better use the new api

--- DW1.21/display.lua
+++ DW1.21/display.lua
@@ -1,97 +1,89 @@
 local function main(jpi)
-
-local mon = peripheral.wrap("right")
-local out = term
-if mon then
-	mon.setTextScale(2)
-	out = mon
-end
-out.clear()
-out.setTextColor(colors.white)
-out.setBackgroundColor(colors.black)
-
-local sizeX, sizeY = out.getSize()
-
-local miningServerID = nil
-
-local function writeCentered(str, line)
-	local x = (sizeX - string.len(str)) / 2
-	out.setCursorPos(x, line)
-	out.write(str)
-end
-
-local function progressBar(top, bot, line)
-	out.setCursorPos(2, line)
-	
-	local length = sizeX - 2
-	for i = 1, length do
-		if (i / length) <= (top / bot) then
-			out.setBackgroundColor(colors.cyan)
-		else
-			out.setBackgroundColor(colors.blue)
-		end
-		
-		out.write(" ")
+	local mon = peripheral.wrap("right")
+	local out = term
+	if mon then
+		mon.setTextScale(2)
+		out = mon
 	end
-	
-	out.setBackgroundColor(colors.black)
-end
-
-while true do
 	out.clear()
-	local line = 1
+	out.setTextColor(colors.white)
+	out.setBackgroundColor(colors.black)
 
-	writeCentered("System Status:", line)
-	line = line + 3
-	
-	if not miningServerID then
-		miningServerID = jpi.arp("MiningServer",1,1)
+	local sizeX, sizeY = out.getSize()
+
+	local miningServerID = nil
+
+	local function writeCentered(str, line)
+		local x = (sizeX - string.len(str)) / 2
+		out.setCursorPos(x, line)
+		out.write(str)
 	end
-	
-	--Make sure it's still there
-	if miningServerID then
-		if not jpi.ping(miningServerID) then
-			miningServerID = nil
-		end
-	end
-	
-	out.setCursorPos(1, line)
-	out.write("MiningServer: ")
-	if not miningServerID then
-		out.setTextColor(colors.red)
-		out.write("Offline")
-		out.setTextColor(colors.white)
-		line = line + 3
-	else
-		out.setTextColor(colors.lime)
-		out.write("Online")
-		out.setTextColor(colors.white)
-		line = line + 2
-	
-		repeat until jpi.send(miningServerID, 0)
-		local id,payload = jpi.receive(miningServerID, 5)
+
+	local function progressBar(top, bot, line)
+		out.setCursorPos(2, line)
 		
-		if id == miningServerID then
-			out.setCursorPos(1, line)
-			out.write("  Turtles: "..payload.turtles)
-			line = line + 1
+		local length = sizeX - 2
+		for i = 1, length do
+			if (i / length) <= (top / bot) then
+				out.setBackgroundColor(colors.cyan)
+			else
+				out.setBackgroundColor(colors.blue)
+			end
 			
-			out.setCursorPos(1, line)
-			out.write("  Idle: "..payload.idle)
+			out.write(" ")
+		end
+		
+		out.setBackgroundColor(colors.black)
+	end
+
+	while true do
+		out.clear()
+		local line = 1
+
+		writeCentered("System Status:", line)
+		line = line + 3
+		
+		if not miningServerID then
+			miningServerID = jpi.arp("MiningServer",1,1)
+		end
+		
+		out.setCursorPos(1, line)
+		out.write("MiningServer: ")
+		
+		if jpi.send(miningServerID, 1, 1) then
+			out.setTextColor(colors.lime)
+			out.write("Online")
+			out.setTextColor(colors.white)
 			line = line + 2
+		
+			repeat until jpi.send(miningServerID, 0)
+			local id,payload = jpi.receive(miningServerID)
 			
-			out.setCursorPos(1, line)
-			out.write("  Progress: "..payload.progress.."/256")
-			line = line + 1
-			
-			progressBar(payload.progress, 256, line)
+			if id == miningServerID then
+				out.setCursorPos(1, line)
+				out.write("  Turtles: "..payload.turtles)
+				line = line + 1
+				
+				out.setCursorPos(1, line)
+				out.write("  Idle: "..payload.idle)
+				line = line + 2
+				
+				out.setCursorPos(1, line)
+				out.write("  Progress: "..payload.progress.."/256")
+				line = line + 1
+				
+				progressBar(payload.progress, 256, line)
+				line = line + 3
+			end
+		else
+			out.setTextColor(colors.red)
+			out.write("Offline")
+			out.setTextColor(colors.white)
 			line = line + 3
 		end
+		
+		os.sleep(10)
 	end
-	
-	os.sleep(10)
-end
-
 end
 
 if jpi then
